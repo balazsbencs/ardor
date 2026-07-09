52e0dcac3b7c8a0543e0732f332f3c610e66a171
Balázs Bencs <balazs.bencs@supercharge.io>
2026-07-09 22:51:14 +0200

    fix: static link LVGL in buildroot build
    Buildroot defaults cmake packages to BUILD_SHARED_LIBS=ON, producing
    liblvgl.so.9 that the package never installs. The app has crash-looped
    with 'cannot open shared object file' since the first image.

--- buildroot/external/package/ardor-pedal/ardor-pedal.mk
+++ buildroot/external/package/ardor-pedal/ardor-pedal.mk
@@ -2,7 +2,9 @@ ARDOR_PEDAL_VERSION = 1.0
 ARDOR_PEDAL_SITE = $(BR2_EXTERNAL_ARDOR_PEDAL_PATH)/../..
 ARDOR_PEDAL_SITE_METHOD = local
 ARDOR_PEDAL_DEPENDENCIES = alsa-lib
-ARDOR_PEDAL_CONF_OPTS = -DCMAKE_BUILD_TYPE=Release -DARDOR_UI_BACKEND=fbdev
+# BUILD_SHARED_LIBS=OFF: Buildroot defaults cmake packages to shared libs,
+# which builds liblvgl.so that never gets installed to the target.
+ARDOR_PEDAL_CONF_OPTS = -DCMAKE_BUILD_TYPE=Release -DARDOR_UI_BACKEND=fbdev -DBUILD_SHARED_LIBS=OFF
 
 define ARDOR_PEDAL_INSTALL_INIT_SYSV
 	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_ARDOR_PEDAL_PATH)/package/ardor-pedal/S99ardor-pedal \
